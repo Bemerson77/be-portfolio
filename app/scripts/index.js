891d3d052bb08991d716bf6030a7ecb8d5826166
Brandon Emerson <Brandon.Emerson77@gmail.com>
2016-05-10 01:32:54 -0400

    header functionality working with jquery: going to use css tran instead

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+var $ = require('jquery');
+
+// hamburger menu function
+$('.menu').click(function(){
+  $(this).toggleClass('open');
+});
+
+
+$('.menu').click(function(){
+  $('.header-inner').slideToggle(300, function(){
+    $('.header-inner').removeClass('hidden');
+  });
+});
+
+$('.menu').click(function(){
+  $('.header-logo').slideToggle(300, function(){
+    $('.header-logo').removeClass('hidden');
+  });
+});
