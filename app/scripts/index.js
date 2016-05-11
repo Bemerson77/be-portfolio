9cc407c9869ec41a8776088eb91808c157f8f96c
Brandon Emerson <Brandon.Emerson77@gmail.com>
2016-05-11 16:49:29 -0400

    header finised with css tran: about laid out

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,16 +3,7 @@ var $ = require('jquery');
 // hamburger menu function
 $('.menu').click(function(){
   $(this).toggleClass('open');
-});
-
-$('.menu').click(function(){
-  $('.header-inner').slideToggle(300, function(){
-    $('.header-inner').removeClass('hidden');
-  });
-});
-
-$('.menu').click(function(){
-  $('.header-logo').slideToggle(300, function(){
-    $('.header-logo').removeClass('hidden');
-  });
+  $('.header-inner').toggleClass('menu-active');
+  $('.header-nav-item').toggleClass('nav-active');
+  $('.header-logo').toggleClass('logo-active');
 });
