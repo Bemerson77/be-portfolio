b3c9cfdf3633b2cb3f9dd5605680aec276c72cc5
Brandon Emerson <Brandon.Emerson77@gmail.com>
2016-05-11 21:52:39 -0400

    updated header nav tran to left right: still working on parallax

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,17 +1,16 @@
 var $ = require('jquery');
 
+// parallax
+// $(document).scroll(function() {
+//   $(".banner-outer").css({
+//     "background-position-y": (-$(this).scrollTop()/20)
+//   })
+// });
+
 // hamburger menu function
 $('.menu').click(function(){
   $(this).toggleClass('open');
   $('.header-inner').toggleClass('menu-active');
   $('.header-nav-item').toggleClass('nav-active');
   $('.header-logo').toggleClass('logo-active');
-});
-
-
-// parallax
-$(document).scroll(function() {
-  $(".banner-outer").css({
-    "background-position-y": (-$(this).scrollTop()/60)
-  })
 });
