b07f0ed5c5d75cc8c9469e510d2c7529c7200840
Brandon Emerson <Brandon.Emerson77@gmail.com>
2016-05-21 02:15:41 -0400

    portfolio section mostly done: not sure about how dark it is still

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -27,8 +27,18 @@ $('.skills-item a').hover(function(e){
 
 $('.project-img').hover(function(e){
   if (e.type === "mouseenter") {
-    $(this).find('a').addClass('project-hover');
+    $(this).find('.project-slide-wrapper').addClass('project-slide-hover');
+    $(this).find('.project-title').addClass('project-hover');
   }else{
-    $(this).find('a').removeClass('project-hover');
+    $(this).find('.project-slide-wrapper').removeClass('project-slide-hover');
+    $(this).find('.project-title').removeClass('project-hover');
+  }
+});
+
+$('.project-img').hover(function(e){
+  if (e.type === "mouseenter") {
+    $(this).find('.project-title').addClass('project-title-hover');
+  }else{
+    $(this).find('.project-title').removeClass('project-title-hover');
   }
 });
